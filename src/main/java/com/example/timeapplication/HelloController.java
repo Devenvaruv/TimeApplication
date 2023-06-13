b682b42d845eddd906ee7f2e4282ae34a071a3f1
your_github_username <devenvaruv@gmail.com>
2023-06-12 22:12:17 -0700

    name changes

--- src/main/java/com/example/timeapplication/HelloController.java
+++ src/main/java/com/example/timeapplication/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.timeapplication;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -8,7 +9,15 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onStartButtonClick() {
+        welcomeText.setText("code for starting the timer");
+    }
+    @FXML
+    protected void onEndButtonClick() {
+       int i = 1;
+       for (int j = 0;j < i; j++){
+           welcomeText.setText("code for ending the timer");
+       }
+
     }
 }
