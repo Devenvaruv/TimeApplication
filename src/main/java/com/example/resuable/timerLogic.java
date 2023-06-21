3d5766fbe4bcfa70cc688f554d88c11a9b96c14e
your_github_username <devenvaruv@gmail.com>
2023-06-20 22:50:20 -0700

    feature updated

--- src/main/java/com/example/resuable/timerLogic.java
+++ src/main/java/com/example/resuable/timerLogic.java
@@ -1,8 +1,37 @@
 package com.example.resuable;
 
 public class timerLogic {
+    private int seconds = 00;
+    private int minutes = 00;
+    private int hours = 00;
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
     public static void start() {
-        System.out.println("start clock logic from here");
+        timerLogic timerlogic = new timerLogic();
+        System.out.println(timerlogic.hours + ":" + timerlogic.hours + ":" + timerlogic.minutes);
     }
 
     public void end() {
