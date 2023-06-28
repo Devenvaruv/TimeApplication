85d2a3892af49c97b9e8ce2b4acb012abd92ef09
your_github_username <devenvaruv@gmail.com>
2023-06-27 22:57:39 -0700

    excel Not Working

--- src/main/java/com/example/timeapplication/HelloApplication.java
+++ src/main/java/com/example/timeapplication/HelloApplication.java
@@ -5,10 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        System.out.println("custom start?? //pogs");
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Timer");
@@ -18,5 +26,45 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+}
+
+class ExcelDataWriter {
+
+    public static void main(String[] args) {
+        String filePath = "C:/poiexcel/Writesheet.xlsx";
+        String sheetName = " Employee Info ";
+
+        try (FileInputStream fileInputStream = new FileInputStream(filePath);
+             Workbook workbook = WorkbookFactory.create(fileInputStream)) {
+
+            Sheet sheet = workbook.getSheet(sheetName);
+
+            // Determine the row index where you want to add the new data
+            int rowIndex = sheet.getLastRowNum() + 1;
+
+            // Create a new row
+            Row newRow = sheet.createRow(rowIndex);
+
+            // Set cell values in the new row
+            Cell cell1 = newRow.createCell(0);
+            cell1.setCellValue("Value 1");
+
+            Cell cell2 = newRow.createCell(1);
+            cell2.setCellValue("Value 2");
+
+            Cell cell3 = newRow.createCell(2);
+            cell3.setCellValue("Value 3");
+
+            // Save the changes to the Excel file
+            try (FileOutputStream fileOutputStream = new FileOutputStream(filePath)) {
+                workbook.write(fileOutputStream);
+            }
+
+            System.out.println("Data added successfully.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.timeapplication {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
 
 
     opens com.example.timeapplication to javafx.fxml;
