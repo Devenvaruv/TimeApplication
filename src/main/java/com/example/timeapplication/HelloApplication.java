71dce90579d8ca6add7255493b3e31221b13dc49
your_github_username <devenvaruv@gmail.com>
2023-07-16 07:10:59 -0700

    v.1.00

--- src/main/java/com/example/timeapplication/HelloApplication.java
+++ src/main/java/com/example/timeapplication/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        System.out.println("custom start??");
+        System.out.println("custom start");
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Timer");
